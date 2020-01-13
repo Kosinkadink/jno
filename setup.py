d088a7b2572f8698b6ddc4fb24e3dcb23c6a0809
Kosinkadink <kosinkadink1@gmail.com>
2020-01-13 14:23:22 -0600

    updated setup.py, removed bin/jno

--- setup.py
+++ setup.py
@@ -5,26 +5,29 @@ install_requirements = open("requirements.txt").read().split('\n')
 #readme from repo
 readme_contents = open("README.md").read()
 
-jno_data = []
-
-jno_version = '0.4.1-dev0'
+jno_version = "0.5.0"
 
 #input into setup
 setup(
-	name='jno',
+	name="jno",
 	version=jno_version,
-	description='Command line interface wrapper for Arduino IDE, inspired by ino',
+	description="Command line interface wrapper for Arduino IDE, inspired by ino",
 	long_description=readme_contents,
-	url = 'https://github.com/kosinkadink/jno',
-	download_url = 'https://github.com/kosinkadink/jno/archive/{}.tar.gz'.format(jno_version),
-	author='Jedrzej Kosinski',
-	author_email='kosinkadink1@gmail.com',
-	license='MIT',
-	keywords='arduino interface wrapper',
-	packages=['jno','jno.commands'],
-	package_data={'jno':jno_data},
+	long_description_content_type="text/markdown",
+	url = "https://github.com/kosinkadink/jno",
+	download_url = "https://github.com/kosinkadink/jno/archive/{}.tar.gz".format(jno_version),
+	author="Jedrzej Kosinski",
+	author_email="kosinkadink1@gmail.com",
+	license="MIT",
+	keywords="arduino interface ide wrapper",
+	packages=["jno"],
+	include_package_data=True,
 	install_requires=install_requirements,
-	scripts=['bin/jno'],
+	entry_ports = {
+		"console_scripts": [
+			"jno=jno.__main__:main",
+		]
+	}
 	classifiers=[
 		"Development Status :: 4 - Beta",
 		"License :: OSI Approved :: MIT License",
